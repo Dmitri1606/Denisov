4f7beddd5d152fc756b3c5774c237b091d003141
Dmitri1606 <dendimvad@gmail.com>
2024-11-28 11:50:30 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,26 @@
+# a=0#втарая задача
+# my_str=str(input())
+# reversed_s = my_str[::-1]
+# print(reversed_s)
+
+# my_str = str(input())#3 задача
+# simv = str(input())
+# index = my_str.find(simv)
+#
+# if index>-1:
+#     print("слово найдено")
+# else:
+#     print("не найдено")
+
+
+# phone=str(input())# 4 задача
+# edited_phone = phone.replace(" ", "_")
+# print(edited_phone)
+
+
+# my_str=str(input())
+# reversed_s = my_str[::-1]
+# if my_str==reversed_s:
+#     print('это палиндром')
+# else:
+#     print('не палиндром')
