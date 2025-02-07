6a3760125ad30e9fa7f62dd7c45cfc31158bb976
Dmitri1606 <dendimvad@gmail.com>
2025-02-07 11:14:23 +0700

    Add files via upload

--- pythonProject16/main.py
+++ pythonProject16/main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
-
-# Чтение Excel-файла
+#
+#
+# # Чтение Excel-файла
 df_excel = pd.read_excel('Книга 3 (1).xlsx')
-
-# Посмотреть первые 5 строк
+#
+ # Посмотреть первые 5 строк
 print(df_excel.head(5))
 
 # Посмотреть последние 5 строк
@@ -12,4 +12,13 @@ print(df_excel.tail(5))
 
 # Посмотреть рандомные 5 строк
 print(df_excel.sample(n=5))
+
+# Посмотреть одну строку
+print(df_excel.iloc[:, 1])
+
+# Посмотреть её среднее значение
+average_value = df_excel.iloc[:, 1].mean()
+print(average_value)
+
+
 
