0f743a4c5798e5dbd564067b09fcac84f530a30c
Dmitri1606 <dendimvad@gmail.com>
2025-02-28 11:49:31 +0700

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,50 +1,20 @@
-from Token import my_token
-import telebot
 import pandas as pd
-import os
+import matplotlib.pyplot as plt
 
-bot = telebot.TeleBot(my_token)
+# Шаг 1: Считываем данные из Excel
+file_path = 'Книга 3 (1) (4).xlsx'  # Путь к файлу Excel
+df = pd.read_excel(file_path)
 
-DOWNLOAD_DIR = "downloads"
-if not os.path.exists(DOWNLOAD_DIR):
-    os.makedirs(DOWNLOAD_DIR)
+# Проверяем структуру данных
+mean_Clas = df['Clas'].mean()
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Привет! Отправь мне файл Excel.")
+a=len(df)
+for i in  df['Clas']:
+    res= i-mean_Clas
+    cvad=res**2
 
-@bot.message_handler(content_types=['document'])
-def handle_document(message):
-    try:
-        if message.document.mime_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-            file_info = bot.get_file(message.document.file_id)
-            downloaded_file = bot.download_file(file_info.file_path)
-            file_name = message.document.file_name
-            file_path = os.path.join(DOWNLOAD_DIR, file_name)
 
-            with open(file_path, 'wb') as f:
-                f.write(downloaded_file)
 
-            bot.reply_to(message, f"Файл '{file_name}' успешно загружен.")
+total_sum = cvad.sum()
 
-            try:
-                df_excel = pd.read_excel(file_path)
-                table_string = df_excel.to_string()  # Преобразуем всю таблицу в строку
-
-                # Разбиваем строку на части, чтобы поместиться в ограничение Telegram
-                chunk_size = 5000
-                for i in range(0, len(table_string), chunk_size):
-                    chunk = table_string[i:i + chunk_size]
-                    bot.send_message(message.chat.id, chunk)
-
-            except FileNotFoundError:
-                bot.send_message(message.chat.id, f"Ошибка: Файл '{file_path}' не найден.")
-            except Exception as e:
-                bot.send_message(message.chat.id, f"Ошибка при чтении Excel файла (Pandas): {e}")
-
-        else:
-            bot.reply_to(message, "Извини, я принимаю только файлы Excel (.xlsx).")
-    except Exception as e:
-        bot.reply_to(message, f"Произошла ошибка при обработке файла: {e}")
-
-bot.infinity_polling()
+print(total_sum/a)
