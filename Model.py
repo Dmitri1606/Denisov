f130c96c018eaec1541c839984eb29e5a40106f2
Dmitri1606 <dendimvad@gmail.com>
2025-04-03 09:28:54 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,16 +1,15 @@
 
-# Model (Модель) - отвечает за данные и логику работы с ними
-books = ["Война и мир", "Гарри Поттер", "Мастер и Маргарита"]
 
+# Model (Модель) - отвечает за данные и логику работы с ними
 def summ(a,b):
     result = a+b
     return result
 def multiplication(a,b):
     sen= a*b
     return sen
-def remove_book(index):
-    if 0 <= index < len(books):
-        books.pop(index)
-
-def get_books():
-    return books
+def Division (a,b):
+    Divisionn= a/b
+    return Divisionn
+def Subtraction(a,b):
+    Subtractionn =a-b
+    return Subtractionn
