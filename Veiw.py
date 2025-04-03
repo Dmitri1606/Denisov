e2f61d896c3065c2ffa80cdce8e52251a7ab1b98
Dmitri1606 <dendimvad@gmail.com>
2025-04-03 09:29:39 +0700

    Update Veiw.py

--- Veiw.py
+++ Veiw.py
@@ -1,9 +1,17 @@
 # View (Представление) - отвечает за отображение и ввод данных
 def display_menu():
-    print("\nВыбери действие: 1 - Умножить, 2 - Сложить, 3 - Поделить")
+    print("\nВыбери действие: 1 - Сложить, 2 - Умножить, 3 - Поделить, 4 - Вычитание")
     choice = input("Введи номер действия: ")
 
     return choice
+
+def get_integer_input(prompt):
+    while True:
+        user_input = input(prompt)
+        try:
+            return int(user_input)
+        except ValueError:
+            print("Ошибка: введенное значение не является целым числом. Попробуйте снова.")
 
 def show_result(result):
     print(result)
@@ -12,13 +20,22 @@ def get_book_title():
     return input("Введи название книги: ")
 
 def get_num():
-    a = int(input("Введи первое число: "))
-    b = int(input("Введи второе число: "))
+    a = get_integer_input("Введи первое число: ")
+    b = get_integer_input("Введи второе число: ")
     return a,b
 
 def get_multiplication():
-    a = int(input("Введи первое число: "))
-    b = int(input("Введи второе число: "))
+    a = get_integer_input("Введи первое число: ")
+    b = get_integer_input("Введи второе число: ")
+    return a,b
+def get_Division():
+    a = get_integer_input("Введи первое число: ")
+    b = get_integer_input("Введи второе число: ")
+    return a, b
+def get_Subtraction ():
+    a = get_integer_input("Введи первое число: ")
+    b = get_integer_input("Введи второе число: ")
+
     return a,b
 
 def show_message(message):
