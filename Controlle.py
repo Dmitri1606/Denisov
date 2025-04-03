2896a74c3f156c94c96a9b66db66a28124b1f66c
Dmitri1606 <dendimvad@gmail.com>
2025-04-03 09:28:37 +0700

    Update Controlle.py

--- Controlle.py
+++ Controlle.py
@@ -6,37 +6,36 @@ def main():
     while True:
         choice = display_menu()
 
+
         if choice == "1":
             a, b = get_num()
 
             show_result(summ(a, b))
 
         elif choice == "2":
-                a,b = get_multiplication()
+            a, b = get_multiplication()
 
-                show_result(multiplication(a,b))
+            show_result(multiplication(a,b))
+
+
+
 
         elif choice == "3":
-            book_list = get_books()
-            show_books(book_list)
-            index_input = get_book_index()
-            try:
-                index = int(index_input)
-                if remove_book(index):
-                    show_message("Книга удалена")
-                elif len(books)<index:
-                    show_message("Такого индекса нет")
-            except ValueError:
-                show_message("Введи корректный номер")
+            a,b= get_Division()
+            if b==0:
+             print("На ноль делить нельзя")
+             main()
+            else:
+             show_result(Division(a, b))
+        elif choice=='4':
+            a,b = get_Subtraction()
+
+            show_result(Subtraction(a, b))
+
         elif choice == "0":
             show_message("Пока!")
             break
-        else:
-            show_message("Неверный выбор, попробуй снова")
+
 
 
 main()
-
-# # Запуск программы
-# if __name__ == "__main__":
-#     main()
