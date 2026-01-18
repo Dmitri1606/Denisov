2309a54355771b7e296a9022abb4e687997965ae
Dmitri1606 <dendimvad@gmail.com>
2026-01-18 13:21:32 +0700

    Add files via upload

---
+++ Proekt_pervoe_polygod.py
@@ -0,0 +1,29 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+
+data = [38, 27, 43, 3, 9, 82, 10]
+sorted_data = merge_sort(data)
+print(f"Отсортированный массив: {sorted_data}")  # Вывод: [3, 9, 10, 27, 38, 43, 82]
